41aae2948240b2c54487e45aa3b3420108ba26e5
higuc <higuc@LAPTOP-RRFEJOT6>
2022-05-23 23:08:29 +0900

    データベース関連のファイルを追加

---
+++ src/controllers/IndexServlet.java
@@ -0,0 +1,35 @@
+package controllers;
+
+import java.io.IOException; //入出力処理中の例外を管理するクラス
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class IndexServlet
+ */
+@WebServlet("/index")
+public class IndexServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public IndexServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//文字列を連結する–appendメソッド
+	}
+
+}
